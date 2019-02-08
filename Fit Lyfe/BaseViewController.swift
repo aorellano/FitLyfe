4e3a7c3a9e6fd460ef05acf8ebb710e0df71c349
Me <alexisorellano20@gmail.com>
2019-02-08 16:47:20 -0600

    Background image for app

--- Fit Lyfe/BaseViewController.swift
+++ Fit Lyfe/BaseViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class BaseViewController: UIViewController {
+    
+    let backgroundImage = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupBackground()
+    }
+    
+    func setupBackground(){
+        backgroundImage.image = UIImage(named: "backgroundImage")
+        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImage.contentMode = .scaleAspectFill
+        
+        view.addSubview(backgroundImage)
+        
+        backgroundImage.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
     }
 
 
